7d4b351c7cbf59b58e6e070d53b3d95cf19247c5
Alejandro <alejandroacho@hotmail.com>
2020-09-03 01:56:10 +0200

    Views | Simple view added

--- src/server.js
+++ src/server.js
@@ -1,17 +1,25 @@
 const express = require ('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 app.set('port', process.env.PORT || 4000 );
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+  }));
+app.set('view engine','.hbs');
 
 app.use(express.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index')
 });
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
